631fd3873fbfdaaca52bacebf21ea0d42043928d
RanduQuintero <randuquintero@hotmail.com>
2026-02-02 00:29:24 -0700

    Prediccion de precio dado un dato

--- Modelo_Regresion.py
+++ Modelo_Regresion.py
@@ -18,15 +18,8 @@ df.columns = df.columns.str.lower() #Minusculas
 df.columns = df.columns.str.replace(" ", "_") #Remplazar espacios en blanco
 df.columns = df.columns.str.replace("-", "_") #Remplazar guion por guion bajo
 
-"""Hacer un cambio de nombre de las columnas
-df = df.rename(columns={
-    "bedrooms": "cuartos",
-    "bathrooms": "banos", #Evitar la Ñ
-    "stories": "pisos",
-    "parking": "cochera",
-    "price": "precio"
-})"""
-#One-Hot_Encoding
+#One_Hot_Encoding para que el modelo se un poco mas eficiente
+#Asignacion de un valor de 1 y 0 para elemntos de si y no
 df = pd.get_dummies(
     df,
     columns=[
@@ -41,6 +34,7 @@ df = pd.get_dummies(
     drop_first=True
 )
 
+#Separacionnde la etiqueta de los datos predictivos
 Datos_entrada = df.drop(columns="price")
 Datos_etiqueta = df["price"]
 
@@ -58,16 +52,43 @@ modelo = LinearRegression()
 modelo.fit(Datos_train_escalados, y_train)
 
 predicciones = modelo.predict(Datos_test_escalados)
-print(predicciones)
+#print(predicciones)
 print(mean_squared_error(y_test, predicciones))
-print("R2:", r2_score(y_test, predicciones))
+print("R2:", r2_score(y_test, predicciones)) #estimacion de que tan bueno es el modelo
+                                             #entre mas cerca del 1, es mejor
 
 plt.scatter(y_test, predicciones)
 plt.plot(
     [y_test.min(), y_test.max()],
     [y_test.min(), y_test.max()]
 )
+
 plt.xlabel("Precio real")
 plt.ylabel("Precio predicho")
 plt.title("Real vs Predicción")
 plt.show()
+
+
+#Dato a predecir su precio que ciertas caracteristicas
+nueva_casa = pd.DataFrame(columns=Datos_entrada.columns)
+nueva_casa.loc[0] = 0  # inicializar todo en 0
+
+#Asignacion de valores 
+nueva_casa.loc[0, "area"] = 5800
+nueva_casa.loc[0, "bedrooms"] = 3
+nueva_casa.loc[0, "bathrooms"] = 2
+nueva_casa.loc[0, "stories"] = 5
+nueva_casa.loc[0, "parking"] = 1
+
+#Asignacion a unas categóricas (One_Hot_Encoding), las demas quedaran en 0
+nueva_casa.loc[0, "mainroad_yes"] = 1
+nueva_casa.loc[0, "airconditioning_yes"] = 1
+nueva_casa.loc[0, "furnishingstatus_semi-furnished"] = 1
+
+#Escalar con el MISMO scaler
+nueva_casa_escalada = scaler.transform(nueva_casa)
+
+#Predicción
+precio_estimado = modelo.predict(nueva_casa_escalada)
+
+print("Precio estimado de la casa:", precio_estimado[0])
